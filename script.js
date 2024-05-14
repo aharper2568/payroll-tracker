00d397799efc66782b2f2a7d4482cdd754a7b280
aharper2568 <anthonyharper568@gmail.com>
2024-05-14 18:41:58 -0400

    added prompting for first name last name and salary

--- script.js
+++ script.js
@@ -2,9 +2,30 @@
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
-const collectEmployees = function() {
+const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
-}
+
+  const employees = [];
+
+  
+    const firstName = prompt('First Name:');
+    const lastName = prompt('Last Name');
+    let salaryInput = prompt("Enter employee's salary:");
+
+   
+
+    employees.push({
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
+    })
+
+
+  
+
+  
+  return employees;
+};
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
